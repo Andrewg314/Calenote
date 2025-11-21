68b4c9478f36f63409dcb76f82ea7d893d73dd76
Andrewg314 <andrewguo108.dog@gmail.com>
2025-11-21 15:39:53 -0500

    finished user_repository db queries for now

--- server/repositories/user_repository.py
+++ server/repositories/user_repository.py
@@ -3,7 +3,7 @@ from db import engine
 
 class UserRepository:
     def create_user(self, user):
-        with engine.connect() as sesh:
+        with engine.begin() as sesh:
             sesh.execute(
                 text("INSERT INTO users (username, password, first_name, last_name) VALUES (:u, :p, :f, :l)"),
                 {
@@ -16,18 +16,42 @@ class UserRepository:
             
             sesh.commit()
 
+    def username_exists(self, username):
+        with engine.connect() as sesh:
+            result = sesh.execute(
+                text("SELECT 1 FROM users WHERE username = :u"),
+                {"u": username}
+            ).fetchone()
+
+        return result is not None
+
     def get_user_credentials(self, username): 
         with engine.connect() as sesh:
             result = sesh.execute(
                 text("SELECT user_id, password FROM users WHERE username = :username"),
                 {"username": username}
-            )
-            row = result.mappings().fetchone()
+            ).mappings().fetchone()
 
-        if not row:
+        if not result:
             return None
         
         return {
-            "user_id": row["user_id"],
-            "hashed_pw": row["password"]
+            "user_id": result["user_id"],
+            "hashed_pw": result["password"]
+        }
+    
+    def get_user_by_id(self, user_id):
+        with engine.connect() as sesh:
+            result = sesh.execute(
+                text("SELECT username, first_name, last_name FROM users WHERE user_id = :user_id"),
+                {"user_id": user_id}
+            ).mappings().fetchone()
+
+        if result is None:
+            return None
+
+        return {
+            "username": result["username"],
+            "first_name": result["first_name"],
+            "last_name": result["last_name"]
         }
